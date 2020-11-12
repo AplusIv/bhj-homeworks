7ab59587d0e27024e2e140434498139a8792c1e9
AplusIv <50slash50@gmail.com>
2020-11-12 17:50:22 +0300

    second commit js-features

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,15 +4,42 @@ let cookie = document.getElementById('cookie');
 
 let counter = document.getElementById('clicker__counter');
 
+let speedometer = document.getElementById('clicker__speedometer');
+let momentTimestamp;
+
+let arr = [];
+
 function countClicks() {
+  momentTimestamp = new Date().getTime();
+  console.log(momentTimestamp);
   counter.textContent++;
   if (counter.textContent % 2 > 0) {
-    this.width = 300;
+    this.width = 300;    
   } else {
     this.width = 200;
   }
+
+  
+  /* ??? Может быть, не самый изящный способ, но считает:) Как можно было не используя массив решить? Как по-другому запоминать нужные таймстемпы? */
+  arr.push(momentTimestamp);
+  console.log(arr);
+
+  if (arr.length > 1) {
+    let lastClick = arr[arr.length - 1];
+    // ??? Почему-то вернулся undefined при записи arr[length - 1], хотя я до этого всегда таким образом писал вывод последнего элемента
+    console.log(`last = ${arr[arr.length - 1]}`);
+    let previousClick = arr[arr.length - 2];
+    console.log(`previous = ${arr[arr.length - 2]}`);
+    let diffSeconds = (lastClick - previousClick) / 1000;
+    console.log(diffSeconds);
+    speedometer.textContent = (1 / diffSeconds).toFixed(2);
+    console.log(speedometer.textContent);
+    
+    arr.shift(); // Можно убирать устаревающие таймстемпы.
+    console.log(arr);
+  }
+  // return momentTimestamp;
 }
 cookie.onclick = countClicks;
-// ??? Если быстро кликать мышкой не срабатывает изменение размеров. Если медленно - всё ок. Почему так?
 
-// ??? Не совсем понимаю, что нужно делать в дополнительной задаче, где нужно замерять скорость клика.Как замерять время, которое прошло после клика? Оно ведь произвольное.
+
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,13 +1,22 @@
 'use strict';
-let hole;
+
 let deadStatus = document.getElementById('dead');
 let lostStatus = document.getElementById('lost');
 
-// ????? Не получается решить задачу, используя цикл. Если напрямую задаю индекс или просто создаю переменную для каждой лунки - всё работает. Как быть?
-/* let getHole = index => document.getElementById(`hole${index}`);
-for (index = 1; index < 10; index++) {
-  getHole(index).onclick = function inspectHole() {
-    if (getHole(index).className.includes('hole_has-mole')) {
+// ??? Решил через заполнение массива. Изящнее способа не придумал:) Это хороший вариант?
+
+const holesArray = [];
+
+for (let index = 1; index < 10; index++) {
+  const getHole = index => document.getElementById(`hole${index}`);
+  holesArray.push(getHole(index));
+}
+
+console.log(holesArray);
+
+for (let hole of holesArray) {
+  hole.onclick = () => {
+    if (hole.className.includes('hole_has-mole')) {
       deadStatus.textContent++;
         if (deadStatus.textContent >= 10) {
           alert('Вы одолели кротов!');
@@ -21,207 +30,6 @@ for (index = 1; index < 10; index++) {
         deadStatus.textContent = 0;
         lostStatus.textContent = 0;
       }
-    }
-  }
-}
-*/
-
-
-
-// let getHole = index => document.getElementById(`hole${index}`);
-/*let getHole = index => {
-  for (index = 1; index < 10; index++) {
-    return document.getElementById(`hole${index}`);
-  }
-  //return document.getElementById(`hole${index}`);
-}
-*/
-/* let getHole;
-for (index = 1; index < 10; index++) {
-  getHole = index => document.getElementById(`hole${index}`);
-}
-
-hole = getHole(index);
-*/
-//hole = document.getElementById(`hole1`)
-
-//hole.onclick = getHole(index);
-
-/* if (hole.onclick().className.includes('hole_has-mole')) {
-  deadStatus++;
-} else {
-  lostStatus++;
-}
-*/
-
-let hole1 = document.getElementById(`hole1`);
-let hole2 = document.getElementById(`hole2`);
-let hole3 = document.getElementById(`hole3`);
-let hole4 = document.getElementById(`hole4`);
-let hole5 = document.getElementById(`hole5`);
-let hole6 = document.getElementById(`hole6`);
-let hole7 = document.getElementById(`hole7`);
-let hole8 = document.getElementById(`hole8`);
-let hole9 = document.getElementById(`hole9`);
-
-hole1.onclick = function inspectHole() {
-  if (hole1.className.includes('hole_has-mole')) {
-    deadStatus.textContent++;
-      if (deadStatus.textContent >= 10) {
-        alert('Вы одолели кротов!');
-        deadStatus.textContent = 0;
-        lostStatus.textContent = 0;
-      }
-  } else {
-    lostStatus.textContent++;
-    if (lostStatus.textContent >= 5) {
-      alert('Кроты оказались сильнее!');
-      deadStatus.textContent = 0;
-      lostStatus.textContent = 0;
-    }
-  }
-}
-
-hole2.onclick = function inspectHole() {
-  if (hole2.className.includes('hole_has-mole')) {
-    deadStatus.textContent++;
-      if (deadStatus.textContent >= 10) {
-        alert('Вы одолели кротов!');
-        deadStatus.textContent = 0;
-        lostStatus.textContent = 0;
-      }
-  } else {
-    lostStatus.textContent++;
-    if (lostStatus.textContent >= 5) {
-      alert('Кроты оказались сильнее!');
-      deadStatus.textContent = 0;
-      lostStatus.textContent = 0;
-    }
-  }
-}
-
-hole3.onclick = function inspectHole() {
-  if (hole3.className.includes('hole_has-mole')) {
-    deadStatus.textContent++;
-      if (deadStatus.textContent >= 10) {
-        alert('Вы одолели кротов!');
-        deadStatus.textContent = 0;
-        lostStatus.textContent = 0;
-      }
-  } else {
-    lostStatus.textContent++;
-    if (lostStatus.textContent >= 5) {
-      alert('Кроты оказались сильнее!');
-      deadStatus.textContent = 0;
-      lostStatus.textContent = 0;
-    }
-  }
-}
-
-hole4.onclick = function inspectHole() {
-  if (hole4.className.includes('hole_has-mole')) {
-    deadStatus.textContent++;
-      if (deadStatus.textContent >= 10) {
-        alert('Вы одолели кротов!');
-        deadStatus.textContent = 0;
-        lostStatus.textContent = 0;
-      }
-  } else {
-    lostStatus.textContent++;
-    if (lostStatus.textContent >= 5) {
-      alert('Кроты оказались сильнее!');
-      deadStatus.textContent = 0;
-      lostStatus.textContent = 0;
-    }
-  }
-}
-
-hole5.onclick = function inspectHole() {
-  if (hole5.className.includes('hole_has-mole')) {
-    deadStatus.textContent++;
-      if (deadStatus.textContent >= 10) {
-        alert('Вы одолели кротов!');
-        deadStatus.textContent = 0;
-        lostStatus.textContent = 0;
-      }
-  } else {
-    lostStatus.textContent++;
-    if (lostStatus.textContent >= 5) {
-      alert('Кроты оказались сильнее!');
-      deadStatus.textContent = 0;
-      lostStatus.textContent = 0;
-    }
-  }
-}
-
-hole6.onclick = function inspectHole() {
-  if (hole6.className.includes('hole_has-mole')) {
-    deadStatus.textContent++;
-      if (deadStatus.textContent >= 10) {
-        alert('Вы одолели кротов!');
-        deadStatus.textContent = 0;
-        lostStatus.textContent = 0;
-      }
-  } else {
-    lostStatus.textContent++;
-    if (lostStatus.textContent >= 5) {
-      alert('Кроты оказались сильнее!');
-      deadStatus.textContent = 0;
-      lostStatus.textContent = 0;
-    }
-  }
-}
-
-hole7.onclick = function inspectHole() {
-  if (hole7.className.includes('hole_has-mole')) {
-    deadStatus.textContent++;
-      if (deadStatus.textContent >= 10) {
-        alert('Вы одолели кротов!');
-        deadStatus.textContent = 0;
-        lostStatus.textContent = 0;
-      }
-  } else {
-    lostStatus.textContent++;
-    if (lostStatus.textContent >= 5) {
-      alert('Кроты оказались сильнее!');
-      deadStatus.textContent = 0;
-      lostStatus.textContent = 0;
-    }
-  }
-}
-
-hole8.onclick = function inspectHole() {
-  if (hole8.className.includes('hole_has-mole')) {
-    deadStatus.textContent++;
-      if (deadStatus.textContent >= 10) {
-        alert('Вы одолели кротов!');
-        deadStatus.textContent = 0;
-        lostStatus.textContent = 0;
-      }
-  } else {
-    lostStatus.textContent++;
-    if (lostStatus.textContent >= 5) {
-      alert('Кроты оказались сильнее!');
-      deadStatus.textContent = 0;
-      lostStatus.textContent = 0;
-    }
-  }
-}
-
-hole9.onclick = function inspectHole() {
-  if (hole9.className.includes('hole_has-mole')) {
-    deadStatus.textContent++;
-      if (deadStatus.textContent >= 10) {
-        alert('Вы одолели кротов!');
-        deadStatus.textContent = 0;
-        lostStatus.textContent = 0;
-      }
-  } else {
-    lostStatus.textContent++;
-    if (lostStatus.textContent >= 5) {
-      alert('Кроты оказались сильнее!');
-      deadStatus.textContent = 0;
-      lostStatus.textContent = 0;
     }
   }
 }
