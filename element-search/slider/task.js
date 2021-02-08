df1812b0bc57e83fc397c97c26d2a96864c784a7
AplusIv <50slash50@gmail.com>
2021-02-08 15:59:11 +0300

    second commit slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,13 +1,13 @@
 'use strict';
 
+// Я переписал в соответствии с рекомендациями. Надеюсь, правильно уловил вашу мысль про разделение логики и излишнее дублирование)
+
 const sliderItems = [...document.querySelectorAll('.slider__item')];
-
 const sliderDots = [...document.querySelectorAll('.slider__dot')];
-
 const sliderArrows = document.querySelector('.slider__arrows');
 
 //Активный слайд
-let activeIndex = sliderItems.findIndex(item => item.classList.contains('slider__item_active') === true);
+let activeIndex = sliderItems.findIndex(item => item.classList.contains('slider__item_active'));
 sliderDots[activeIndex].classList.add('slider__dot_active');
 
 // Слушатели
@@ -16,50 +16,45 @@ for (let index = 0; index < sliderDots.length; index++) {
 }
 sliderArrows.addEventListener('click', swipeSlides);
 
+function showSlide(slideIndex) {
+  sliderItems[activeIndex].classList.toggle('slider__item_active');
+  sliderItems[slideIndex].classList.toggle('slider__item_active');
+  activeIndex = slideIndex;
+};
 
+function chooseSlide() {
+  let chosenDot = sliderDots.indexOf(this);
+  switchDotsVisuals(chosenDot);
+  showSlide(chosenDot);
+  activeIndex = chosenDot;
+};
 
-//Точки
-function chooseSlide(event) {
-  let index = sliderDots.indexOf(event.target);
-  for (let sliderItem of sliderItems) {
-    sliderItem.className = 'slider__item';
-  }
-   for (let sliderDot of sliderDots) {
-    sliderDot.className = 'slider__dot';
-  }
-  sliderItems[index].classList.add('slider__item_active');
-  event.target.classList.add('slider__dot_active');
-}
-
-//??? Стоит ли как-то упростить код? Всё работает как нужно, но я не уверен в правильности моего подхода)
-
-// Стрелочки
 function swipeSlides(event) {
-  let activeIndex = sliderItems.findIndex(item => item.classList.contains('slider__item_active') === true);
-
   if (event.target.classList.contains('slider__arrow_next')) {
-    sliderItems[activeIndex].classList.remove('slider__item_active');
-    sliderDots[activeIndex].classList.remove('slider__dot_active');
-    if (sliderItems[activeIndex] === sliderItems[sliderItems.length - 1]) {      
-      activeIndex = 0;
-      sliderItems[activeIndex].classList.add('slider__item_active');
-      sliderDots[activeIndex].classList.add('slider__dot_active'); 
+    let nextSlideIndex;
+    if (activeIndex === sliderItems.length - 1) {
+      nextSlideIndex = 0;
     } else {
-      sliderItems[activeIndex + 1].classList.add('slider__item_active');
-      sliderDots[activeIndex + 1].classList.add('slider__dot_active'); 
-    }
+      nextSlideIndex = activeIndex + 1;
+    }    
+    switchDotsVisuals(nextSlideIndex);    
+    showSlide(nextSlideIndex);
+    activeIndex = nextSlideIndex;
   }
-
   if (event.target.classList.contains('slider__arrow_prev')) {
-    sliderItems[activeIndex].classList.remove('slider__item_active');
-    sliderDots[activeIndex].classList.remove('slider__dot_active');
-    if (sliderItems[activeIndex] === sliderItems[0]) {      
-      activeIndex = sliderItems.length - 1;
-      sliderItems[activeIndex].classList.add('slider__item_active');
-      sliderDots[activeIndex].classList.add('slider__dot_active'); 
+    let prevSlideIndex;
+    if (activeIndex === 0) {
+      prevSlideIndex = sliderItems.length - 1;  
     } else {
-      sliderItems[activeIndex - 1].classList.add('slider__item_active');
-      sliderDots[activeIndex - 1].classList.add('slider__dot_active'); 
+      prevSlideIndex = activeIndex - 1;
     }
+    switchDotsVisuals(prevSlideIndex);    
+    showSlide(prevSlideIndex);
+    activeIndex = prevSlideIndex;
   }
-}
+};
+
+function switchDotsVisuals(newSlide) {
+  sliderDots[activeIndex].classList.toggle('slider__dot_active');
+  sliderDots[newSlide].classList.toggle('slider__dot_active');
+}4
